e3f7868c9202cabdca1fd436c403bbec82628aaa
01mehran <132054519+01mehran@users.noreply.github.com>
2024-02-06 22:18:31 +0330

    creat JS

---
+++ script.js
@@ -0,0 +1,24 @@
+const count = document.querySelector("#count");
+const increaseBtn = document.querySelector("#Increase");
+const resetBtn = document.querySelector("#Reset");
+const decreaseBtn = document.querySelector("#Decrease");
+
+let counter = 0;
+increaseBtn.addEventListener("click", () => {
+  counter++;
+  count.innerHTML = counter < 10 ? "0" + counter : counter;
+});
+
+resetBtn.addEventListener("click", () => {
+  counter = "00";
+  count.innerHTML = counter;
+});
+
+decreaseBtn.addEventListener("click", () => {
+  counter--;
+  if (counter < 0) {
+    alert("no");
+    counter = 0;
+  }
+  count.innerHTML = counter < 10 ? "0" + counter : counter;
+});
